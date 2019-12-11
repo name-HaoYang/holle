992a2a61a3b39ecd821ea9c2130325f696e2334e
Administrator <Administrator@EMPJBNHI7WPRIOC>
2019-12-11 12:01:51 +0800

    test

--- src/com/test/Holle.java
+++ src/com/test/Holle.java
@@ -1,10 +1,31 @@
 package com.test;
 
+import com.pojo.User;
+
 public class Holle {
 
 	public static void main(String[] args) {
 		
 		System.out.println("holle");
+		
+		String test = method1();
+		
+		System.out.println(test);
+		
+	}
+	
+	public static String method1() {
+		
+		User user = new User();
+		
+		user.setId(1);
+		user.setUsername("朱");
+		user.setPassword("123456");
+		user.setAge(20);
+		
+		String test = user.toString();
+		
+		return test;
 		
 	}
 
